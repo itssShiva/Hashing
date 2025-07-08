255e0e0c6bb6b06b52759f6c7d778d30201fe93c
itssShiva <iitts.shiva@gmail.com>
2025-07-08 10:21:59 +0530

    Create mostFrequentElement.cpp
    it checks the most frequent element inside array.

---
+++ mostFrequentElement.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of array: "<<endl;
+    cin>>n;
+    unordered_map<int,int>mpp;
+    int arr[n];
+     int max_element=0;
+     int max_freq=0;
+    int most_freq_element;
+    cout<<"Enter all array elements: "<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        mpp[arr[i]]++;
+    }
+
+    cout<<"Ente the number of elements whose frequency you want to check : "<<endl;
+    int q;
+    cin>>q;
+    while(q--){
+         cout<<"Ente the number  whose frequency you want to check : "<<endl;
+        int number;
+        cin>>number;
+        cout<<"Number of frequency of Element you given :"<<endl;
+        cout<<mpp[number];
+    }
+
+   
+}
